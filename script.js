7c33bcd1f41b9fd0d2ae08db4b5b56fd918a2bfb
MiaMint <zineb.biz111@gmail.com>
2026-06-06 01:16:19 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -57,4 +57,50 @@ blueNote.textContent = localStorage.getItem("blue") || "";
 yellowNote.textContent = localStorage.getItem("yellow") || "";
 }
 
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
+const timer = document.getElementById("timer");
 
+let timeLeft = 1200;
+let interval;
+
+const updateTimer = () => {
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft % 60;
+
+    timer.innerHTML = `${minutes.toString().padStart(2,"0")}
+    :
+    ${seconds.toString().padStart(2,"0")}`;
+}
+
+const startTimer = () => {
+    if(interval) {
+        clearInterval(interval);
+    }
+    interval = setInterval(() => {
+        if (timeLeft > 0) {
+            timeLeft--;
+        updateTimer();
+        } else {
+            clearInterval(interval);
+            alert("Good Job!");
+            timeLeft = 1200;
+            updateTimer();
+        }
+    }, 1000);
+};
+
+const stopTimer = () => clearInterval(interval);
+
+const resetTimer = () => {
+    clearInterval(interval);
+    timeLeft = 1200;
+    updateTimer();
+}
+
+start.addEventListener("click", startTimer);
+stop.addEventListener("click", stopTimer);
+reset.addEventListener("click", resetTimer);
+
+updateTimer();
